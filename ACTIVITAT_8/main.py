eef7652eb6faa371af9c6e1c2404417f36022c53
Miilenna <milena200.fv@gmail.com>
2024-11-13 20:02:04 +0100

    reponse error 204

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from typing import Optional
+app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
+    description: str
     price: float
-    tax: float | None = None
-
-
-app = FastAPI()
-
+    tax: float
+    category: str
+    discount: Optional[float] = None
 
 @app.post("/items/")
 async def create_item(item: Item):
     return item
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", status_code=204)
 async def read_item(item_id: int):
     return {"item_id": item_id}
