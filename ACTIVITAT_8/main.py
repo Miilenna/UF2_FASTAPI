b1b38c79dae26774fa8aebb699b4b3a49d4fb053
Miilenna <milena200.fv@gmail.com>
2024-11-13 20:34:29 +0100

    http 204 error

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from fastapi import HTTPException
 app = FastAPI()
 
 class Item(BaseModel):
@@ -10,6 +11,8 @@ class Item(BaseModel):
     tax: float
     category: str
     discount: Optional[float] = None
+    
+items = {"milena": "Milena vardumyan"}
 
 @app.post("/items/")
 async def create_item(item: Item):
@@ -17,4 +20,6 @@ async def create_item(item: Item):
 
 @app.get("/items/{item_id}", status_code=204)
 async def read_item(item_id: int):
+    if item_id not in items:
+        raise HTTPException(status_code=204, detail="No content")
     return {"item_id": item_id}
