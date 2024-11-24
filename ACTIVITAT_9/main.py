f9713891e0b63962c453dc13ca7e3c68d14c835d
Miilenna <milena200.fv@gmail.com>
2024-11-24 15:40:09 +0100

    main fet

--- ACTIVITAT_9/main.py
+++ ACTIVITAT_9/main.py
@@ -1,12 +1,17 @@
-from pydantic import BaseModel
+# Importa las clases necesarias desde FastAPI y otros módulos
 from typing import List
 from fastapi import FastAPI
-from users import read_users
-from schemas import users_schema
+from crud.users import read_users
+from schemas.schemas import users_schema
 
+# Crea una instancia de la aplicación FastAPI
 app = FastAPI()
 
-@app.get("/users", response_model=List[dict])
+# Define una ruta GET para obtener todos los usuarios
+@app.get("/users/", response_model=List[dict])
 async def get_users():
+    # Llama a la función read_users para obtener la lista de usuarios
     users = read_users()
+    
+    # Devuelve los usuarios serializados con la función 'users_schema'
     return users_schema(users)
