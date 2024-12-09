256f3f31e1be22ab85788091f5325b1d4696b33b
Miilenna <milena200.fv@gmail.com>
2024-12-09 18:57:09 +0100

    commit de main, read y schemas a classe

--- ACTIVITAT_11/main.py
+++ ACTIVITAT_11/main.py
@@ -3,21 +3,21 @@ app = FastAPI()
 
 intents = 0
 
+# 1
 @app.get("/començar")
 def començar():
     return { 
             "text":"Començar partida"
             }
-    
+# 2
 @app.get("/començar/paraula")
 def comença_paraula():
     return { 
             "text":"Començar partida"
             }
-
+# 3
 @app.post("/intents")
 def intents():
-    intents += 1
     return {
         intents
     }
