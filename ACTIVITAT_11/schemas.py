519c93027f8d0f3ad34abba4e5d35e88492eb9eb
Miilenna <milena200.fv@gmail.com>
2024-12-15 12:15:15 +0100

    comentaris afegits

--- ACTIVITAT_11/schemas.py
+++ ACTIVITAT_11/schemas.py
@@ -1,6 +1,6 @@
-# Función que devuelve una lista de diccionarios con una clave 'option' y el valor correspondiente a 'paraula'
+# Función que devuelve una lista de diccionarios con una clave 'text' y el valor correspondiente a 'paraula'
 def paraula_schema(paraula) -> list:
     return [{"text":paraula}]
-# Función que devuelve una lista de diccionarios a partir de una lista de temáticas
+# Función que devuelve una lista de diccionarios a partir de una lista
 def word_schema(words) -> dict:
     return [{"text": word[0]} for word in words]
